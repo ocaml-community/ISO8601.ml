d134a811011ce48e19b0f02444293d2772c39d55
Julien Sagot <ju.sagot@gmail.com>
2015-02-17 19:31:06 +0100

    Moved API in a Permissive module (it is!).

--- src/ISO8601.ml
+++ src/ISO8601.ml
@@ -1,17 +1,21 @@
 module Lexer = ISO8601_lexer
 
-let date_lex lexbuf = Lexer.date lexbuf
+module Permissive = struct
 
-let time_lex lexbuf =
-  let t = Lexer.time lexbuf in
-  match Lexer.timezone lexbuf with None -> t | Some o -> t +. o
+    let date_lex lexbuf = Lexer.date lexbuf
 
-let datetime_lex lexbuf =
-  let d = Lexer.date lexbuf in
-  match Lexer.delim lexbuf with
-  | Some _ -> d +. Lexer.time lexbuf
-  | _        -> assert false
+    let time_lex lexbuf =
+      let t = Lexer.time lexbuf in
+      match Lexer.timezone lexbuf with None -> t | Some o -> t +. o
 
-let date s = date_lex (Lexing.from_string s)
-let time s = time_lex (Lexing.from_string s)
-let datetime s = datetime_lex (Lexing.from_string s)
+    let datetime_lex lexbuf =
+      let d = Lexer.date lexbuf in
+      match Lexer.delim lexbuf with
+      | Some _ -> d +. Lexer.time lexbuf
+      | _        -> assert false
+
+    let date s = date_lex (Lexing.from_string s)
+    let time s = time_lex (Lexing.from_string s)
+    let datetime s = datetime_lex (Lexing.from_string s)
+
+end
