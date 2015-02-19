b2d4363dba82974c89b3fa5925bd24d4bb321d6e
Julien Sagot <ju.sagot@gmail.com>
2015-02-19 08:43:00 +0100

    Added reqtime optionnal argument to datetime function.

--- src/ISO8601.ml
+++ src/ISO8601.ml
@@ -8,14 +8,15 @@ module Permissive = struct
       let t = Lexer.time lexbuf in
       match Lexer.timezone lexbuf with None -> t | Some o -> t +. o
 
-    let datetime_lex lexbuf =
+    let datetime_lex ?(reqtime=true) lexbuf =
       let d = Lexer.date lexbuf in
       match Lexer.delim lexbuf with
       | Some _ -> d +. Lexer.time lexbuf
-      | _        -> assert false
+      | _      -> if reqtime then assert false else d
 
     let date s = date_lex (Lexing.from_string s)
     let time s = time_lex (Lexing.from_string s)
-    let datetime s = datetime_lex (Lexing.from_string s)
+    let datetime ?(reqtime=true) s =
+      datetime_lex ~reqtime:reqtime (Lexing.from_string s)
 
 end
