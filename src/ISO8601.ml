08e71e58ad991908a576aa086e43f247850b6292
Julien Sagot <ju.sagot@gmail.com>
2015-02-19 11:34:20 +0100

    Added string_of_* and pp_* functions.

--- src/ISO8601.ml
+++ src/ISO8601.ml
@@ -19,4 +19,34 @@ module Permissive = struct
     let datetime ?(reqtime=true) s =
       datetime_lex ~reqtime:reqtime (Lexing.from_string s)
 
+    let pp_date fmt x =
+      let open Unix in
+      let x = gmtime x in
+      Format.fprintf fmt "%04d%02d-%02d"
+                     (x.tm_year + 1900) (x.tm_mon + 1) x.tm_mday
+
+    let pp_time fmt x =
+      let open Unix in
+      let x = gmtime x in
+      Format.fprintf fmt "%02d:%02d:%02dZ"
+                     x.tm_hour x.tm_min x.tm_sec
+
+    let pp_datetime fmt x =
+      let open Unix in
+      let x = gmtime x in
+      Format.fprintf fmt "%04d%02d-%02dT%02d:%02d:%02dZ"
+                     (x.tm_year + 1900) (x.tm_mon + 1) x.tm_mday
+                     x.tm_hour x.tm_min x.tm_sec
+
+    let string_of_aux printer x =
+      ignore (Format.flush_str_formatter ()) ;
+      printer Format.str_formatter x ;
+      Format.flush_str_formatter ()
+
+    let string_of_date = string_of_aux pp_date
+
+    let string_of_time = string_of_aux pp_time
+
+    let string_of_datetime = string_of_aux pp_datetime
+
 end
