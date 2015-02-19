9f1aa2d316a411938baed1b641cb1be24c16dba8
Julien Sagot <ju.sagot@gmail.com>
2015-02-19 16:38:32 +0100

    Optionally keep and print timezone.

--- src/ISO8601.ml
+++ src/ISO8601.ml
@@ -4,18 +4,34 @@ module Permissive = struct
 
     let date_lex lexbuf = Lexer.date lexbuf
 
-    let time_lex lexbuf =
+    let time_tz_lex lexbuf =
       let t = Lexer.time lexbuf in
-      match Lexer.timezone lexbuf with None -> t | Some o -> t -. o
+      let tz = Lexer.timezone lexbuf in
+      let t = match tz with None -> t | Some o -> t -. o in
+      (t, tz)
+
+    let datetime_tz_lex ?(reqtime=true) lexbuf =
+      let d = date_lex lexbuf in
+      match Lexer.delim lexbuf with
+      | None -> if reqtime then assert false else (d, None)
+      | Some _ -> let (t, tz) = time_tz_lex lexbuf in
+                  (d +. t, tz)
+
+    let time_lex lexbuf =
+      fst (time_tz_lex lexbuf)
 
     let datetime_lex ?(reqtime=true) lexbuf =
-      let d = Lexer.date lexbuf in
-      match Lexer.delim lexbuf with
-      | Some _ -> d +. Lexer.time lexbuf
-      | _      -> if reqtime then assert false else d
+      fst (datetime_tz_lex ~reqtime:reqtime lexbuf)
 
     let date s = date_lex (Lexing.from_string s)
+
     let time s = time_lex (Lexing.from_string s)
+
+    let time_tz s = time_tz_lex (Lexing.from_string s)
+
+    let datetime_tz ?(reqtime=true) s =
+      datetime_tz_lex ~reqtime:reqtime (Lexing.from_string s)
+
     let datetime ?(reqtime=true) s =
       datetime_lex ~reqtime:reqtime (Lexing.from_string s)
 
@@ -25,18 +41,28 @@ module Permissive = struct
       Format.fprintf fmt "%04d%02d-%02d"
                      (x.tm_year + 1900) (x.tm_mon + 1) x.tm_mday
 
-    let pp_time fmt x =
-      let open Unix in
-      let x = gmtime x in
-      Format.fprintf fmt "%02d:%02d:%02dZ"
-                     x.tm_hour x.tm_min x.tm_sec
+    let pp_time_tz_aux t tz =
+      let (t, tz) =
+        match tz with
+        | None -> (t, "")
+        | Some 0. -> (t, "Z")
+        | Some x -> (t +. x, Format.sprintf "%2.0f:%2.0f"
+                                            (x /. 3600.)
+                                            (abs_float (x /. 60.))) in
+      (Unix.gmtime t, tz)
 
-    let pp_datetime fmt x =
+    let pp_time ?(tz=None) fmt x =
       let open Unix in
-      let x = gmtime x in
-      Format.fprintf fmt "%04d%02d-%02dT%02d:%02d:%02dZ"
+      let (x, tz) = pp_time_tz_aux x tz in
+      Format.fprintf fmt "%02d:%02d:%02d%s"
+                     x.tm_hour x.tm_min x.tm_sec tz
+
+    let pp_datetime ?(tz=None) fmt  x =
+      let open Unix in
+      let (x, tz) = pp_time_tz_aux x tz in
+      Format.fprintf fmt "%04d%02d-%02dT%02d:%02d:%02d%s"
                      (x.tm_year + 1900) (x.tm_mon + 1) x.tm_mday
-                     x.tm_hour x.tm_min x.tm_sec
+                     x.tm_hour x.tm_min x.tm_sec tz
 
     let string_of_aux printer x =
       ignore (Format.flush_str_formatter ()) ;
@@ -45,8 +71,8 @@ module Permissive = struct
 
     let string_of_date = string_of_aux pp_date
 
-    let string_of_time = string_of_aux pp_time
+    let string_of_time ?(tz=None) = string_of_aux (pp_time ~tz:tz)
 
-    let string_of_datetime = string_of_aux pp_datetime
+    let string_of_datetime ?(tz=None) = string_of_aux (pp_datetime ~tz:tz)
 
 end
--- src/ISO8601.mli
+++ src/ISO8601.mli
@@ -1,3 +1,5 @@
+(** {2 ISO 8601 and RFC 3339 parsing and printing} *)
+
 module Permissive : sig
 
     (** {2 Date parsing}
@@ -5,22 +7,34 @@ module Permissive : sig
         [date], [time] and [datetime] parse a [string] and return the
         corresponding timestamp (as [float]).
 
+        Times are {b always} converted to UTC representation.
+
         For each of these functions, a [_lex] suffixed function read
         from a [Lexing.lexbuf] instead of a [string].
 
         [datetime] functions also take an optional boolean [reqtime]
         indicating if parsing must fail if a date is given and not
         a complete datetime. (Default is true).
+
+        Functions with [_tz] in their name return a [float * float option]
+        representing [timestamp * offset option (timezone)]. [timestamp]
+        will be the UTC time, and [offset option] is just an information
+        about the original timezone.
      *)
 
     val date_lex : Lexing.lexbuf -> float
     val time_lex : Lexing.lexbuf -> float
     val datetime_lex : ?reqtime:bool -> Lexing.lexbuf -> float
 
+    val time_tz_lex : Lexing.lexbuf -> (float * float option)
+    val datetime_tz_lex : ?reqtime:bool -> Lexing.lexbuf -> (float * float option)
+
     val date : string -> float
     val time : string -> float
     val datetime : ?reqtime:bool -> string -> float
 
+    val time_tz : string -> (float * float option)
+    val datetime_tz : ?reqtime:bool -> string -> (float * float option)
 
     (** {2 Printing functions}
 
@@ -30,18 +44,23 @@ module Permissive : sig
         [string_of_X] functions return a string representation
         of a [X].
 
+        Times relative function take an optionnal timezone argument.
+
         [X] define the format used:
         - date: [ YYYY-MM-DD ]
-        - time: [ hh:mm:ssZ ]
-        - datetime: [ YYYY-MM-DDThh:mm:ssZ ]
+        - time: [ hh:mm:ss ]
+        - datetime: [ YYYY-MM-DDThh:mm:ss ]
+
+        {b NB: fractionnal part of timestamps will be lost when printing
+         with current implementation.}
      *)
 
     val pp_date : Format.formatter -> float -> unit
-    val pp_time : Format.formatter -> float -> unit
-    val pp_datetime : Format.formatter -> float -> unit
+    val pp_time : ?tz:float option -> Format.formatter -> float -> unit
+    val pp_datetime : ?tz:float option -> Format.formatter -> float -> unit
 
     val string_of_date : float -> string
-    val string_of_time : float -> string
-    val string_of_datetime : float -> string
+    val string_of_time : ?tz:float option -> float -> string
+    val string_of_datetime : ?tz:float option -> float -> string
 
 end
