5d6a74275a0613fa53c7355e545de7e6d7b0dc7d
Julien Sagot <ju.sagot@gmail.com>
2015-03-03 13:47:47 +0100

    Moved mk functions, into Utils module.

--- ISO8601_TEST.ml
+++ ISO8601_TEST.ml
@@ -1,22 +1,4 @@
-let mkdate y m d =
-  { Unix.tm_sec = 0 ; tm_min = 0 ; tm_hour = 0 ;
-    tm_wday = -1 ; tm_yday = -1 ;tm_isdst = false ;
-    tm_mday = d ;
-    tm_mon = m - 1 ;
-    tm_year = y - 1900 ; }
-  |> Unix.mktime
-  |> fst
-
-let mkdatetime y m d h mi s =
-  { Unix.tm_sec = s ; tm_min = mi ; tm_hour = h ;
-    tm_wday = -1 ; tm_yday = -1 ;tm_isdst = false ;
-    tm_mday = d ;
-    tm_mon = m - 1 ;
-    tm_year = y - 1900 ; }
-  |> Unix.mktime
-  |> fst
-
-let mktime h m s = h *. 3600. +. m *. 60. +. s
+open Utils
 
 let test p fn input expected =
   let result = fn input in
---
+++ utils.ml
@@ -0,0 +1,12 @@
+let mkdatetime y m d h mi s =
+  { Unix.tm_sec = s ; tm_min = mi ; tm_hour = h ;
+    tm_wday = -1 ; tm_yday = -1 ;tm_isdst = false ;
+    tm_mday = d ;
+    tm_mon = m - 1 ;
+    tm_year = y - 1900 ; }
+  |> Unix.mktime
+  |> fst
+
+let mkdate y m d = mkdatetime y m d 0 0 0
+
+let mktime h m s = h *. 3600. +. m *. 60. +. s
